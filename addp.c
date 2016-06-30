4ff6dddbf2e7410ebc5ba5d846d5196fad7507af
Nandni1995 <nandnis95@gmail.com>
2016-06-30 14:19:34 +0530

    Create addp.c

---
+++ addp.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a=0,r,t,t1,sum=0;
+clrscr();
+printf("Enter the number");
+scanf("%d",&n);
+t=n;
+while(t!=0)
+{
+r=t%10;
+a=a+r;
+t=t/10;
+}
+printf("%d\n",a);
+t1=a;
+while(a)
+{
+r=a%10;
+a=a/10;
+sum=sum*10+r;
+}
+if(t1==sum)
+{
+printf("palindrome");
+}
+else
+printf("Not a palindrome");
+getch();
+}
+
